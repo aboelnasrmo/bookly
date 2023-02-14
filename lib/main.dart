1199a2d4cd7f395de32523e8b5f61c38c8f1aa9e
mohammadaboelnasr <m.aboelnasr@live.com>
2023-02-14 19:30:19 +0300

    create splash view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'Features/splash/presentation/views/splash_view.dart';
+
 void main() {
   runApp(const MyApp());
 }

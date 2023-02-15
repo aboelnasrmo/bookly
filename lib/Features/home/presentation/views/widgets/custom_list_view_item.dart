1515bc97cb4d76acfd215ec69a943ad561cb2452
mohammadaboelnasr <m.aboelnasr@live.com>
2023-02-15 21:50:45 +0300

    Custom list view item added

---
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/const/const.dart';
+import '../../../../../core/const/my_assets.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      width: 120,
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(16),
+        ),
+        shape: BoxShape.rectangle,
+        border: Border.all(width: 2, color: kMyWhite),
+        color: Colors.orange.shade500,
+      ),
+      alignment: Alignment.topLeft,
+      child: Container(
+        height: 170,
+        width: 95,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadiusDirectional.only(
+            bottomStart: Radius.zero,
+            topStart: Radius.circular(16),
+            topEnd: Radius.circular(16),
+            bottomEnd: Radius.circular(16),
+          ),
+          shape: BoxShape.rectangle,
+          color: Colors.orange.shade200,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 10, right: 10),
+          child: Image.asset(MyAssets.logo),
+        ),
+      ),
+    );
+  }
+}
--- lib/core/const/const.dart
+++ lib/core/const/const.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const Color kMyBackGround = Color(0xff100B20);
 const kTransitionDuration = Duration(seconds: 2);
+const Color kMyBookBackGroundColor = Colors.orange;
+const Color kMyWhite = Colors.white;
